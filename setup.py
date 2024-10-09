1cb01b19ec584c13b648fd8192e5845f43e9d3f3
sooyoung-wind <sooyoung.wind@gmail.com>
2024-10-10 00:17:31 +0900

    🐛 fix information

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(
         'pandas',
         'numpy',
         'torch',
-        # 필요한 다른 패키지들을 여기에 추가하세요
     ],
     description='A collection of utility functions by Soo',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/your_username/soo_functions_project',
+    author='Sooyoung Her',
+    author_email='sooyoung.wind@gmail.com',
+    url='https://github.com/sooyoung-wind/soo_functions',
 )
