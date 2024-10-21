278dbf901402a5259b746e96a6f5b1c8d3a280e3
SooY <sooyoung.wind@gmail.com>
2024-10-21 13:13:30 +0900

    Update __init__.py
    statistic의 중복 이름으로 인해 변경합니다.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .soo_functions import resumetable, seed_everything
-from .statistic import me, mae, rmse, d, trans_to_UV, trans_to_WSWD, wd_diff_cal, getmode, mean_wd, wd_statistic, soo_statistic
+from .soo_statistic import me, mae, rmse, d, trans_to_UV, trans_to_WSWD, wd_diff_cal, getmode, mean_wd, wd_statistic, soo_statistic
 
 __all__ = [
     "resumetable",
